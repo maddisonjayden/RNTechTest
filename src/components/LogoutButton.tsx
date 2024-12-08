3d4e9ffdb1186de5e34b13885af1847f81253d3d
Jayden Maddison <jayden.maddison@gmail.com>
2024-12-08 21:48:54 +0000

    Fix logout issue

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -4,22 +4,23 @@ import { Button } from 'react-native';
 import { useDispatch } from 'react-redux';
 
 import { Paths } from '@/navigation/paths';
-
 import { resetAccount } from '@/api';
 import { queryClient } from '@/App';
-import { logoutUser } from '@/redux/actions/userActions';
+import { logout } from '@/redux/actions/authActions';
 
 export default function LogoutButton() {
   const dispatch = useDispatch();
   const navigation = useNavigation<any>();
+  
   const handleLogout = async () => {
     await resetAccount();
     await queryClient.clear();
-    await dispatch(logoutUser());
+    await dispatch(logout());
     navigation.reset({
       index: 0,
       routes: [{ name: Paths.Login }],
     });
   };
+  
   return <Button title="Logout" onPress={handleLogout} />;
 }
