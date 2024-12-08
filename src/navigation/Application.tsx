538d87877d015c2d138e053ed50b36000e71b691
Jayden Maddison <jayden.maddison@gmail.com>
2024-12-08 21:47:25 +0000

    Add RootState

--- src/navigation/Application.tsx
+++ src/navigation/Application.tsx
@@ -1,28 +1,48 @@
-import type { RootStackParamList } from '@/navigation/types';
-
+import { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { useDispatch, useSelector } from 'react-redux';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import { useTheme } from '@/theme';
 import { Paths } from '@/navigation/paths';
-
 import { Home, Login } from '@/screens';
+import { authService } from '@/services/authService';
+import { loginSuccess, logout } from '@/redux/actions/authActions';
+import { RootState } from '@/redux/reducers';
 
-const Stack = createStackNavigator<RootStackParamList>();
+const Stack = createStackNavigator();
 
 function ApplicationNavigator() {
   const { navigationTheme, variant } = useTheme();
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector((state: RootState) => state.auth.isLoggedIn);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      const tokens = authService.getTokens();
+      const user = authService.getUser();
+      
+      if (tokens && user && !authService.isTokenExpired(tokens)) {
+        dispatch(loginSuccess(user, tokens));
+      } else {
+        dispatch(logout());
+      }
+    };
+
+    checkAuth();
+  }, [dispatch]);
 
   return (
     <SafeAreaProvider>
       <NavigationContainer theme={navigationTheme}>
         <Stack.Navigator
           key={variant}
-          screenOptions={{ headerShown: false, gestureEnabled: true }}
+          screenOptions={{ headerShown: false }}
+          initialRouteName={isLoggedIn ? Paths.Home : Paths.Login}
         >
-          <Stack.Screen component={Login} name={Paths.Login} />
-          <Stack.Screen component={Home} name={Paths.Home} />
+          <Stack.Screen name={Paths.Login} component={Login} />
+          <Stack.Screen name={Paths.Home} component={Home} />
         </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
